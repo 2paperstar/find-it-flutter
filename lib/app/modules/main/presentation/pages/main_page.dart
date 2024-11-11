49b8319a542e4dae085f0ab72881e6d93727340d
2paperstar <lee@paperst.ar>
2024-11-11 09:23:49 +0900

    feat: add dummy data

--- lib/app/modules/main/presentation/pages/main_page.dart
+++ lib/app/modules/main/presentation/pages/main_page.dart
@@ -12,11 +12,26 @@ class MainPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('find-it'),
       ),
-      bottomNavigationBar: BottomNavigationBar(items: const [
-        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-        BottomNavigationBarItem(icon: Icon(Icons.message), label: 'Messages'),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-      ]),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: const InputDecoration(
+                hintText: 'Search',
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          BottomNavigationBar(items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.message), label: 'Messages'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ]),
+        ],
+      ),
       body: Stack(
         children: [
           Container(
@@ -24,19 +39,54 @@ class MainPage extends StatelessWidget {
             child: const Center(child: Text('Map')),
           ),
           DraggableScrollableSheet(
-            builder: (context, scrollController) => Container(
-              color: Palette.white,
-              child: SingleChildScrollView(
-                controller: scrollController,
-                child: const Column(
-                  children: [
-                    ListTile(title: Text('test1')),
-                    ListTile(title: Text('test1')),
-                    ListTile(title: Text('test1')),
-                    ListTile(title: Text('test1')),
-                  ],
+            builder: (context, scrollController) => Column(
+              children: [
+                Container(
+                  height: 10,
+                  color: Colors.black,
+                  child: Center(
+                    child: Container(
+                        width: 50,
+                        height: 5,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5),
+                        )),
+                  ),
                 ),
-              ),
+                Expanded(
+                  child: Container(
+                    color: Palette.white,
+                    child: SingleChildScrollView(
+                      controller: scrollController,
+                      child: const Column(
+                        children: [
+                          ListTile(
+                            title: Text('Wallet'),
+                            subtitle: Text('2학생회관'),
+                            trailing: Text('0.2km'),
+                          ),
+                          ListTile(
+                            title: Text('Wallet'),
+                            subtitle: Text('대학 A동'),
+                            trailing: Text('0.4km'),
+                          ),
+                          ListTile(
+                            title: Text('Card'),
+                            subtitle: Text('대학 A동'),
+                            trailing: Text('0.5km'),
+                          ),
+                          ListTile(
+                            title: Text('Phone'),
+                            subtitle: Text('대학 B동'),
+                            trailing: Text('0.5km'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
